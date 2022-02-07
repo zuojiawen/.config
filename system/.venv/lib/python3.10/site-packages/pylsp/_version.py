c52c73710b85f0c45870a6328dd24707b94a6ed3
zuojiawen <2035865201@qq.com>
2022-02-07 16:16:15 +0800

    system

---
+++ system/.venv/lib/python3.10/site-packages/pylsp/_version.py
@@ -0,0 +1,7 @@
+# Copyright 2017-2020 Palantir Technologies, Inc.
+# Copyright 2021- Python Language Server Contributors.
+
+"""PyLSP versioning information."""
+
+VERSION_INFO = (1, 3, 3)
+__version__ = '.'.join(map(str, VERSION_INFO))
---
+++ system/.venv/lib/python3.10/site-packages/pyright/cli.py
@@ -0,0 +1,46 @@
+import os
+import sys
+import logging
+import subprocess
+from typing import List, NoReturn, Union, Any
+
+from . import node
+from .utils import env_to_bool
+
+
+__all__ = (
+    'run',
+    'main',
+)
+
+log: logging.Logger = logging.getLogger(__name__)
+
+
+def main(args: List[str], **kwargs: Any) -> int:
+    return run(*args, **kwargs).returncode
+
+
+def run(
+    *args: str, **kwargs: Any
+) -> Union['subprocess.CompletedProcess[bytes]', 'subprocess.CompletedProcess[str]']:
+    version = os.environ.get('PYRIGHT_PYTHON_FORCE_VERSION')
+    if version is None:
+        version = node.latest('pyright')
+
+    npx = node.version('npx')
+    if npx[0] >= 7:
+        pre_args = ['--yes']
+
+        if not env_to_bool('PYRIGHT_PYTHON_VERBOSE', default=False):
+            pre_args.insert(0, '--silent')
+    else:
+        pre_args = []
+
+    if args and pre_args:
+        pre_args = (*pre_args, '--')
+
+    return node.run('npx', *pre_args, f'pyright@{version}', *args, **kwargs)
+
+
+def entrypoint() -> NoReturn:
+    sys.exit(main(sys.argv[1:]))
